8b1137cc6111406f28352622d93428a536693ac2
jromo29 <44422850+jromo29@users.noreply.github.com>
2019-03-08 00:56:21 -0500

    Add files via upload

--- JavaScript&DOMManipulation/static/js/app.js
+++ JavaScript&DOMManipulation/static/js/app.js
@@ -5,69 +5,92 @@ var tableData = data;
 
 
 var table = d3.select("tbody");
-
 var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var filteredDataArray = []
 
-var inputField = d3.select("#datetime");
+function createTable(array) {
 
-var inputValue = inputField.property("value");
-
-
-//console.log(inputFilter)
-
-//var tableRow = table.select("tr");
-
-//tableRow.append("td").text("data1");
-
-//console.log(data);
-
-
-
-data.forEach(element => {
-
-    var tableRow = table.append("tr");
+    array.forEach(element => {
     
-    Object.entries(element).forEach(([key,value]) => {
+        var tableRow = table.append("tr");
         
-        var tableCell = tableRow.append("td").text(value);
+        Object.entries(element).forEach(([key,value]) => {
+            
+            var tableCell = tableRow.append("td").text(value);
+        });
     });
-});
+}
 
-function dateFilter() {
+createTable(tableData);
+
+//Filter Functions:
+function dateFilter(array, filterCriteria) {
     
-    // Stop Page Refresh
-    d3.event.preventDefault();
-    //console.log("Button Works");
-    var inputValue = inputField.property("value");
-    //console.log(inputValue);
+    filterDataArray = array.filter(sighting => sighting.datetime == filterCriteria);
+    return  filterDataArray
+}
 
-    filterDataArray = tableData.filter(sighting => sighting.datetime == inputValue);
-    //console.log(filterDataArray);
+function cityFilter(array,filterCriteria) {
+
+    filterDataArray = array.filter(sighting => sighting.city == filterCriteria);
+    return  filterDataArray
+}
+
+function createTable2(array) {
 
     //Delete all current table data
-
     table.text("");
-
-    if (filterDataArray.length !== 0) {
     
-        //input new table data
-        filterDataArray.forEach(element => {
+    console.log(array.length);
+    console.log(array);
 
-            var tableRow = table.append("tr");
-            
-            Object.entries(element).forEach(([key,value]) => {
-                
-                var tableCell = tableRow.append("td").text(value);
-            });
-        });
+    if (array.length !== 0) {
+        createTable(array);
+        
     } else {
         tableRow = table.append("tr").text("NO MATCHING RESULTS")
     }
+}
+
+
+function filterResults() {
+    
+    // Stop Page Refresh
+    d3.event.preventDefault();
+        
+    var inputValueDate = inputFieldDate.property("value");
+    //console.log(inputValueDate);
+    
+    var inputValueCity = inputFieldCity.property("value");
+    //console.log(inputValueCity)
+
+    if (inputValueDate !== "" && inputValueCity !== "") {
+     
+        var df1 = dateFilter(tableData, inputValueDate);
+        var df2 = cityFilter(df1,inputValueCity);
+        createTable2(df2);
+        
+    
+    }else if(inputValueCity !== "") {
+            
+        var df2 = cityFilter(tableData,inputValueCity);
+        createTable2(df2);
+    }else if (inputValueDate !== "") {
+            
+        var df1 = dateFilter(tableData,inputValueDate);
+        createTable2(df1);
+
+    }
+    
     //Clean input field after button is pressed
-    inputField.property("value","");
+    inputFieldDate.property("value","");
+    inputFieldCity.property("value","");
 
-};
+}
 
-button.on("click", dateFilter);
+button.on("click", filterResults);
+
 
 
